4ff640ecc95e0910b837a23506bffd04f2e2d89d
flohreh <159389587+flohreh@users.noreply.github.com>
2024-05-06 13:50:59 -0400

    Update data-gen.py

--- data-gen.py
+++ data-gen.py
@@ -1,6 +1,7 @@
 import sqlite3
 import os
 
+# Database file path
 DATABASE = '/nfs/demo.db'
 
 def connect_db():
@@ -13,7 +14,8 @@ def generate_test_data(num_contacts):
     for i in range(num_contacts):
         name = f'Test Name {i}'
         phone = f'123-456-789{i}'
-        db.execute('INSERT INTO contacts (name, phone) VALUES (?, ?)', (name, phone))
+        email = f'test_email_{i}@example.com'  # Add email address
+        db.execute('INSERT INTO contacts (name, phone, email) VALUES (?, ?, ?)', (name, phone, email))  # Include email in INSERT statement
     db.commit()
     print(f'{num_contacts} test contacts added to the database.')
     db.close()
