a4528202a6b9848fdad5483294116ee2dd14544f
flohreh <159389587+flohreh@users.noreply.github.com>
2024-05-06 14:18:53 -0400

    Update data-gen.py

--- data-gen.py
+++ data-gen.py
@@ -11,6 +11,9 @@ def connect_db():
 def generate_test_data(num_contacts):
     """Generate test data for the contacts table."""
     db = connect_db()
+    # Add the "email" column to the table if it doesn't exist
+    db.execute('''ALTER TABLE contacts ADD COLUMN email TEXT''')
+    db.commit()
     for i in range(num_contacts):
         name = f'Test Name {i}'
         phone = f'123-456-789{i}'
@@ -19,6 +22,7 @@ def generate_test_data(num_contacts):
     db.commit()
     print(f'{num_contacts} test contacts added to the database.')
     db.close()
+
 
 if __name__ == '__main__':
     generate_test_data(10)  # Generate 10 test contacts.
